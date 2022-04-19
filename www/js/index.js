45a88151eb94060e1431d37e40e1e9f7e8fa56b9
JD Runtsch <katman04@gmail.com>
2022-04-19 15:59:26 -0500

    JS for points and codes added. p1 info added

--- www/js/index.js
+++ www/js/index.js
@@ -28,28 +28,79 @@ function onDeviceReady() {
     document.getElementById('deviceready').classList.add('ready');
 } */
 
- window.onclick = e => {
-     console.log(e.target);  // to get the element
-    /* console.log(e.target.className);  // to get the element tag name alone */    
-    let numHit;
-    if (e.target.className == 'pBox'){
-        if (e.target.style.height != "400px"){ //expands princess selection to 400px tall, then displays more info about princess
-        numHit = e.target.id; //get id
-        console.log('pbox hits');
-        e.target.style.animation = "expand 600ms forwards";
-        e.target.style.height = "400px";
+let pVal = 0;
+let cVal = 0;
+window.onclick = (e) => {
+  //console.log(button); // to get the element
+  /* console.log(e.target.className);  // to get the element tag name alone */
+  //console.log(e.target.className);
+
+  //Next section will add or subtract succession points -- all point ids start with 7
+  if (e.target.id == 71) {
+    //add 1 point
+    pVal++;
+    document.getElementById("pNumGame").innerHTML = pVal;
+  } else if (e.target.id == 72) {
+    //add 2 points
+    pVal = pVal + 2;
+    document.getElementById("pNumGame").innerHTML = pVal;
+  } else if (e.target.id == 73) {
+    //add 3 points
+    pVal = pVal + 3;
+    document.getElementById("pNumGame").innerHTML = pVal;
+  } else if (e.target.id == 76) {
+    //add 6 points
+    pVal = pVal + 6;
+    document.getElementById("pNumGame").innerHTML = pVal;
+  } else if (e.target.id == 711) {
+    //subtract 1 point
+    pVal--;
+    document.getElementById("pNumGame").innerHTML = pVal;
+  } else if (e.target.id == 712) {
+    //subtract 2 points
+    pVal = pVal - 2;
+    document.getElementById("pNumGame").innerHTML = pVal;
+  }
+
+  //Next section will add coins -- all coin ids starrt with 8
+  if (e.target.id == 81) {
+    //add 1 point
+    cVal++;
+    document.getElementById("cNumTurn").innerHTML = cVal;
+  } else if (e.target.id == 82) {
+    //add 2 points
+    cVal = cVal + 2;
+    document.getElementById("cNumTurn").innerHTML = cVal;
+  } else if (e.target.id == 83) {
+    //add 3 points
+    cVal = cVal + 3;
+    document.getElementById("cNumTurn").innerHTML = cVal;
+  } else if (e.target.id == 800) {
+    //add 3 points
+    cVal = 0;
+    document.getElementById("cNumTurn").innerHTML = cVal;
+  }
+
+  //next section for princessPage only
+  if (document.URL.includes("princessPage.html")) {
+    //make sure princess page is active page
+    if (e.target.id == 1 || e.target.id == 11 || e.target.id == 111) {
+      if (document.getElementById("1").style.height != "60vh") {
+        //check height
+        //expands princess 1 selection to 400px tall, then displays more info about princess 1
+        document.getElementById("1").style.animation = "expand 600ms forwards";
+        document.getElementById("1").style.height = "60vh";
         tname = e.target.className;
-    } 
-
-    else if (e.target.style.height == "400px"){ //closes expanded info
-
-        e.target.style.animation = "close 600ms forwards";
-        e.target.style.height = "10vh";
-            console.log( e.target.className + " works " + e.target.style.height)
-            
-        }
-        
-        
+        document.getElementById("111").style.display = "flex";
+        document.getElementById("bigInfo1").style.display = "block";
+      }
+    } else {
+      //close if expanded
+      document.getElementById("1").style.animation = "close 600ms forwards";
+      document.getElementById("1").style.height = "1.6vh";
+      document.getElementById("111").style.display = "none";
+      document.getElementById("bigInfo1").style.display = "none";
     }
-}  
-
+  } else {
+  }
+};
